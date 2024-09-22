e39b5f60946252e578f0c90f89c650cf7dde2606
Avishek Barua <avishekbarua02@gmail.com>
2024-09-22 11:59:14 +0600

    project section done

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 function openPage(pageName, event) {
-    let i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("content");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].classList.remove("active");
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].classList.remove("active");
-    }
-    document.getElementById(pageName).classList.add("active");
-    event.currentTarget.classList.add("active");
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].classList.remove("active");
   }
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("defaultOpen").click();
-  });  
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+  document.getElementById(pageName).classList.add("active");
+  event.currentTarget.classList.add("active");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("defaultOpen").click();
+});
